eb37586d414f01423b46169c8554fd1d0297a9d7
Ali Nmb <n.ali75012@gmail.com>
2023-10-03 15:18:06 +0200

    Feat: Intégration Hero

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "../src/style/App.scss";
 import Navbar from "../src/components/Navbar/Navbar";
 import Hero from "../src/components/Hero/Hero";
+import Features from "../src/components/Features/Features";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Features />
     </div>
   );
 }
--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -3,11 +3,23 @@ import headerImg from "/img/bank-tree.jpeg";
 
 const Hero = () => {
   return (
-    <header>
-      <img src={headerImg} />
-      <div>
-        <h2></h2>
-        <p></p>
+    <header className="Hero">
+      <img
+        src={headerImg}
+        alt="Cette photographie représente un verre transparent avec des pièces avec une petite plante qui y pousse au milieu"
+        className="Hero__img"
+      />
+      <div className="Hero__rightBloc">
+        <h2 className="Hero__title">
+          No fees.
+          <br />
+          No minimum deposit. <br />
+          High interest rates.
+        </h2>
+        <p className="Hero__txt">
+          Open a savings account with <br />
+          Argent Bank today!
+        </p>
       </div>
     </header>
   );
