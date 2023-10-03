44f0843007dafba58e9f98f4077d2145bfe04fc6
Ali Nmb <n.ali75012@gmail.com>
2023-10-03 15:56:09 +0200

    Feat: Intégration Features

--- frontend/src/components/Features/Features.jsx
+++ frontend/src/components/Features/Features.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import IconChat from "/img/icon-chat.png";
+import IconMoney from "/img/icon-money.png";
+import IconSecurity from "/img/icon-security.png";
 
 const Features = () => {
-  return <div></div>;
+  const data = [
+    {
+      id: 1,
+      img: IconChat,
+      title: "You are our #1 priority",
+      subtitle:
+        "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
+    },
+    {
+      id: 2,
+      img: IconMoney,
+      title: "More savings means higher rates",
+      subtitle:
+        "The more you save with us, the higher your interest rate will be!",
+    },
+    {
+      id: 3,
+      img: IconSecurity,
+      title: "Security you can trust",
+      subtitle:
+        "We use top of the line encryption to make sure your data and money is always safe.",
+    },
+  ];
+
+  const feat = data.map((item) => {
+    return (
+      <div key={item.id}>
+        <div className="Features__bloc">
+          <img src={item.img} className="Features__img" />
+          <h3 className="Features__title">{item.title}</h3>
+          <p className="Features__subtitle">{item.subtitle}</p>
+        </div>
+      </div>
+    );
+  });
+  return <div className="Features">{feat}</div>;
 };
 
 export default Features;
