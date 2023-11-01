0c7bfee8fbd4e2e1635b16cd5671c78e4d2ed60f
Ali Nmb <n.ali75012@gmail.com>
2023-11-01 11:27:15 +0100

    Refacto: Structure page Login

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,56 +1,13 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
-import { useDispatch } from "react-redux";
-import { userLogin } from "../../actions/user.action";
-import { useNavigate } from "react-router-dom";
+import Form from "../../components/Form/Form";
 
 const Login = () => {
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const form = useRef();
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleForm = async (e) => {
-    e.preventDefault();
-
-    const postData = {
-      email: form.current[0].value,
-      password: form.current[1].value,
-      checked: form.current.querySelector("#remember-me").checked,
-    };
-
-    dispatch(userLogin(postData, navigate, setErrorMessage));
-  };
-
   return (
     <div>
       <Navbar />
-      <main className="Signin">
-        <section className="Signin__form">
-          <i className="fa fa-user-circle Signin__icon"></i>
-          <h1>Sign In</h1>
-          <form ref={form} onSubmit={(e) => handleForm(e)}>
-            <div className="Signin__wrapper">
-              <label htmlFor="email">Email</label>
-              <input type="text" id="email" required />
-            </div>
-            <div className="Signin__wrapper">
-              <label htmlFor="password">Password</label>
-              <input type="password" id="password" required />
-            </div>
-            <div className="Signin__remember">
-              <input type="checkbox" id="remember-me" />
-              <label htmlFor="remember-me">Remember me</label>
-            </div>
-            <p style={errorMessage ? { color: "red" } : null}>{errorMessage}</p>
-            <button className="Signin__btn" type="submit">
-              Sign In
-            </button>
-          </form>
-        </section>
-      </main>
+      <Form />
       <Footer />
     </div>
   );
